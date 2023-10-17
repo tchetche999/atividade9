1c3f9d80e654a73f4d0cb04bd76bc70c9b511fe3
deborasilvaaaa <deborasilvacardosos021@gmail.com>
2023-10-17 10:51:35 -0300

    adicionei atividade

---
+++ chaidisson09.py
@@ -0,0 +1,7 @@
+#faça um software que verifique entre 2 numeros qual o maior
+n1 = int(input("digite um numero"))
+n2 = int(input("digite um numero"))
+if n1 >= n2:
+    print("o numero maior é:" , n1)
+else: 
+    print("o numero maior é" , n2)
